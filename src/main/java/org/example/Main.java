791fd72fadc0be4a58f8a5111b49be4327183ba9
Alexandre Oliveira <alexmdo@gmail.com>
2023-06-27 21:07:37 -0300

    containsDuplicate and singleNumber challenges

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,77 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        var nums = new int[]{2, 2, 1};
+        System.out.println(new Main().singleNumber(nums)); // 1
+
+        nums = new int[]{4, 1, 2, 1, 2};
+        System.out.println(new Main().singleNumber(nums)); // 4
+
+        nums = new int[]{1};
+        System.out.println(new Main().singleNumber(nums)); // 1
+
+    }
+
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                i++;
+                continue;
+            }
+
+            return nums[i];
+        }
+
+        return nums[nums.length - 1];
+    }
+
+    /*
+        var nums = new int[]{1, 2, 3, 1};
+        System.out.println(new Main().containsDuplicate(nums)); // true
+
+        nums = new int[]{1, 2, 3, 4};
+        System.out.println(new Main().containsDuplicate(nums)); // false
+
+        nums = new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println(new Main().containsDuplicate(nums)); // true
+     */
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /*
+        Causes Time Limit Exceed
+
+    @Deprecated
+    public boolean containsDuplicate(int[] nums) {
+        var foundHit = 0;
+        for (int i = 0; i < nums.length; i++) {
+            var outerValue = nums[i];
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                var innerValue = nums[j];
+                if (outerValue == innerValue) {
+                    foundHit++;
+                }
+            }
+        }
+
+        return foundHit > 1;
+    }*/
+
+
+    /*
         var nums = new int[]{1, 2, 3, 4, 5, 6, 7};
         var k = 3;
         new Main().rotate(nums, k);
@@ -13,8 +84,7 @@ public class Main {
         k = 2;
         new Main().rotate(nums, k);
         System.out.println(Arrays.toString(nums)); // [3,99,-1,-100]
-    }
-
+     */
     public void rotate(int[] nums, int k) {
         while (k > 0) {
             var swapElem = nums[nums.length - 1];
