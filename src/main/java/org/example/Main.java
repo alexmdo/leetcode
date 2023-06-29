4d8d54a069b7ae14021e2bbeac62c4bdda8e4fe1
Alexandre Oliveira <alexmdo@gmail.com>
2023-06-29 11:19:41 -0300

    intersect - pending solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,53 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Main().intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2})));  // [2, 2]
+        System.out.println(Arrays.toString(new Main().intersect(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4}))); // [4, 9]
+        System.out.println(Arrays.toString(new Main().intersect(new int[]{3, 1, 2}, new int[]{1, 1}))); // [1]
+    }
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        var aux = new ArrayList<Integer>();
+
+        int[] arr1;
+        int[] arr2;
+        if (nums1.length > nums2.length) {
+            arr1 = nums2;
+            arr2 = nums1;
+        } else {
+            arr1 = nums1;
+            arr2 = nums2;
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            var outerValue = arr1[i];
+            var hasMatch = false;
+            for (int j = 0; j < arr2.length; j++) {
+                if (outerValue == arr2[j]) {
+                    hasMatch = true;
+                    break;
+                }
+            }
+
+            if (hasMatch) {
+                aux.add(outerValue);
+            }
+        }
+
+        var result = new int[aux.size()];
+        for (var i = 0; i < aux.size(); i++) {
+            result[i] = aux.get(i);
+        }
+
+        return result;
+    }
+
+    /*
         var nums = new int[]{2, 2, 1};
         System.out.println(new Main().singleNumber(nums)); // 1
 
@@ -12,9 +56,7 @@ public class Main {
 
         nums = new int[]{1};
         System.out.println(new Main().singleNumber(nums)); // 1
-
-    }
-
+     */
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 1; i++) {
