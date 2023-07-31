aad045182a9a00786e6e5f16c2151a018f4ee6d9
Alexandre Oliveira <alexmdo@gmail.com>
2023-07-30 22:17:09 -0300

    Longest Common Prefix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,35 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(new Main().longestCommonPrefix(new String[]{"flower","flow","flight"})); // "fl"
+        System.out.println(new Main().longestCommonPrefix(new String[]{"dog","racecar","car"})); // ""
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+
+    /*
         System.out.println(new Main().strStr("sadbutsad", "sad")); // 0
         System.out.println(new Main().strStr("leetcode", "leeto")); // -1
         System.out.println(new Main().strStr("aaa", "aaaa")); // -1
         System.out.println(new Main().strStr("mississippi", "issipi")); // -1
-    }
-
+     */
     public int strStr(String haystack, String needle) {
         if (needle.length() > haystack.length()) {
             return -1;
